51ae2d22a01ba9017ccce3ba060f25677f72e9de
nirosha-IOS <nirosha.iosdeveloper@gmail.com>
2024-09-05 09:53:39 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'To-Do List',
         theme: ThemeData(
-          primarySwatch: Color.fromARGB(255, 28, 133, 219),
+          primarySwatch: const Color.fromARGB(255, 28, 133, 219),
         ),
         home: TodoListScreen(),
       ),
